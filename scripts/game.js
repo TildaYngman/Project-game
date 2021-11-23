a716844c424add3252ff258b57ede4eacd8d9596
fagerblom <nora.fagerblom@hyperisland.se>
2021-11-23 13:28:54 +0100

    added random platforms

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,9 @@
 import Phaser from 'phaser'// Phaser through node
 import backgroundimage from "../assets/kitchen.jpg";
 import player from "../assets/cat.png";
+import platform from "../assets/platform.png";
 
-let background, player1, player1Controls; // add block
+let background, player1, player1Controls, platforms; 
 
 export default class Game extends Phaser.Scene {
 
@@ -10,22 +11,31 @@ preload () {
     //What assets does the game need
    this.load.image('background', backgroundimage);
    this.load.image('cat', player);
+   this.load.image('platformPng', platform);
 }
+
 
 create () {
     background = this.add.image(400, 200, 'background');
+
+    // create random platforms
+    platforms = this.physics.add.staticGroup( { //Make a group of the platforms, duplicate and add physics
+      allowGravity: false, //
+      immovable: true, //
+      });
+  
+      for (let i = 0; i < 8; i++) { //Forloop to create 8 random platforms
+        let randomX = Math.floor(Math.random() * 400) + 24;
+        platforms.create(randomX, i * 80, 'platformPng').setScale(.2);
+      }
+
     player1 = this.physics.add.sprite(400, 250, 'cat');
     player1.setScale(0.05);
     player1.setCollideWorldBounds();
     player1.setVelocityY(600);
     player1Controls = this.input.keyboard.createCursorKeys();
 
-    this.physics.world.checkCollision.bottom = true;
-    // this.physics.world.collider = true;
-
-
-    // block = this.add.rectangle(200, 200, 100, 20, 0x000000);
-    //this.tilda() //to call a function within a function, the this refers to the function.
+    this.physics.world.checkCollision.bottom = true; //Checking collison between player and bottom of the world (enable jump)
 }
 
 update () {
