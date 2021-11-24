6e2394299bf4e1f3410ebf72ff2af9162602713a
Tilda Yngman <Matilda.yngman@hyperisland.se>
2021-11-24 13:01:13 +0100

    platform test

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser'// Phaser through node
 import backgroundimage from "../assets/kitchen.jpg";
 import player from "../assets/cat.png";
-import platform from "../assets/platform.png";
+import platform from "../assets/platform2.png";
 
 let background, player1, player1Controls, platforms; 
 
@@ -17,31 +17,22 @@ preload () {
 
 create () {
     background = this.add.image(400, 200, 'background');
-
-    // create random platforms
-    platforms = this.physics.add.staticGroup( { //Make a group of the platforms, duplicate and add physics
-      allowGravity: false, //
-      immovable: true, //
-      });
-  
-      for (let i = 0; i < 8; i++) { //Forloop to create 8 random platforms
-        let randomX = Math.floor(Math.random() * 400) + 24;
-        platforms.create(randomX, i * 80, 'platformPng').setScale(.2);
-      }
+    
+    this.randomPlatforms ()
 
     player1 = this.physics.add.sprite(400, 250, 'cat');
     player1.setScale(0.05);
     player1.setCollideWorldBounds();
+    this.physics.add.collider(player1, platforms);
     player1.setVelocityY(600);
     player1Controls = this.input.keyboard.createCursorKeys();
-
     this.physics.world.checkCollision.bottom = true; //Checking collison between player and bottom of the world (enable jump)
 }
 
 update () {
 
   this.CharacterMovement()
-  // this.characterJump()
+
 }
 
 CharacterMovement () {
@@ -53,19 +44,19 @@ CharacterMovement () {
     player1.setVelocityX(500);
   }
   if (player1Controls.space.isDown && player1.body.onFloor()) {
-    player1.setVelocityY(-300);
+    player1.setVelocityY(-350);
     console.log("space is pressed")
    }
 }
 
-// characterJump () {
-//   if (player1Controls.space.isDown && player1.body.touching.down) {
-//     player1.setVelocityY(-300);
-//     console.log(player1)
-//    }
-// }
+randomPlatforms () { // create random platforms
+  platforms = this.physics.add.staticGroup(); //Make a group of the platforms, duplicate and add physics
 
+    for (let i = 0; i < 8; i++) { //Forloop to create 8 random platforms
+        let randomX = Math.floor(Math.random() * 400) + 24;
+        platforms.create(randomX, i * 80, 'platformPng');
+      }
+   //platforms.setBodySize(80, 280);
+}
 
-
-//tilda(){} creating a function does not need to have function before
 }
