11588d485dfa42ba4b42dcbeb65645aba4fe9c2b
Tilda Yngman <Matilda.yngman@hyperisland.se>
2021-11-25 11:21:21 +0100

    added comments and divided platforms to a function

--- scripts/game.js
+++ scripts/game.js
@@ -18,25 +18,18 @@ preload () {
 create () {
     background = this.add.image(400, 200, 'background');
     
-    platforms = this.physics.add.group( {
-      allowGravity: false, //
-      immovable: true, //
-    });//Make a group of the platforms, duplicate and add physics
-    
-    for (let i = 0; i < 8; i++) { //Forloop to create 8 random platforms
-      let randomX = Math.floor(Math.random() * 800) + 24;
-      platforms.create(randomX, i * 80, 'platformPng');
-    }
+    this.createPlatforms ()
 
     player1 = this.physics.add.sprite(400, 250, 'cat');
     player1.setScale(0.05);
+    player1.setVelocityY(600);
     player1.setCollideWorldBounds();
-    player1.body.checkCollision.up = false;
+    player1.body.checkCollision.up = false; //up, left and right Make it possible for the player to jump through the platforms
     player1.body.checkCollision.left = false;
     player1.body.checkCollision.right = false;
-    this.physics.add.collider(player1, platforms);
-    player1.setVelocityY(600);
+    this.physics.add.collider(player1, platforms);// making a collide between the player and the platforms so the player can stand on top of the platforms
     player1Controls = this.input.keyboard.createCursorKeys();
+
     this.physics.world.checkCollision.bottom = true; //Checking collison between player and bottom of the world (enable jump)
 
 }
@@ -60,4 +53,17 @@ CharacterMovement () {
     // console.log("space is pressed")
    }
 }
+
+createPlatforms () {
+  platforms = this.physics.add.group( {
+    allowGravity: false, //
+    immovable: true, //
+  });//Make a group of the platforms, duplicate and add physics
+  
+  for (let i = 0; i < 8; i++) { //Forloop to create 8 random platforms
+    let randomX = Math.floor(Math.random() * 800) + 24;
+    platforms.create(randomX, i * 80, 'platformPng');
+  }
+}
+
 }
