a9054a23afaff469a2af3318818fa9c7988b84b1
Tilda Yngman <Matilda.yngman@hyperisland.se>
2021-11-29 09:59:57 +0100

    added GameOverScene

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,13 @@ import Phaser, { Physics } from 'phaser';// Phaser through node
 // import Game from './Game';
 import Title from './Title';
 import Game from './Game';
+import GameOver from './GameOver';
 
 var config = {//What the gameboard is going to look like.
     type: Phaser.AUTO,// will choose either webGL or canvas as renderer
     width: 800, //height and width of the game board
     height: 600,
-    scene:[Title, Game], // run preload, create, update
+    scene:[Title, Game, GameOver], // run preload, create, update
     physics: {
         default: 'arcade',
         arcade: {
