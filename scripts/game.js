94db83d1f6705da8a291122ba452f532ff7a56d1
Tilda Yngman <Matilda.yngman@hyperisland.se>
2021-12-01 19:18:07 +0100

    logo and text

--- scripts/game.js
+++ scripts/game.js
@@ -95,6 +95,7 @@ create () {
     this.physics.add.overlap(player1, lava, () => {
       gameOver = this.add.image(400, 300, 'gameover').setOrigin(0.5, 2);
       this.add.text(400, 300, 'Your score is:', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', color: '#00ff00', fontSize:'50px' }).setOrigin(0.5);
+      this.add.text(400, 300, 'Click to play again', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', color: '#00ff00', fontSize:'30px' }).setOrigin(0.5, -2);
       this.physics.pause();
       gameState = false;
       this.anims.pauseAll();
