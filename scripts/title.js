42ad3ffeee75ba10b9b16a907318f1037778adba
fagerblom <nora.fagerblom@hyperisland.se>
2021-12-02 16:48:33 +0100

    lore and start game colour changed

--- scripts/title.js
+++ scripts/title.js
@@ -3,7 +3,7 @@ import Logo from "../assets/ASLogo.png";
 import startTheme from "url:../assets/whogivesafuck.mp3";
 import playerSheet from "../assets/sheet.png";
 
-let positions, Logo, themeMusic;
+let positions, Logo, themeMusic, lore;
 let spaceBackground;
 let player1;
 
@@ -75,8 +75,9 @@ export default class Title extends Phaser.Scene {
 
     //    this.add.text(positions.centerX, positions.centerY, 'AstroCat!').setOrigin(0.5, 0.5), { fontSize: "20px"} ;
 
+        lore = this.add.text(45, 300, 'On the 18th of October in 1963 a stray Parisian cat named Félicette,\n as a part of the French space program, was successfully launched into space\n and thereby became the first (and only!) cat to reach the stars.\n Join her on her outer space odyssey, but watch out for the space lava!', { fontFamily: 'Arial', fill: '#FFFFFF', fontSize: 15, color: "#FFFFFF", align: 'center' }).setDepth(10);
 
-        const startGameButton = this.add.text(positions.centerX, positions.centerY + 100, 'Start Game!', { fontFamily: 'Arial', fill: '#FFFFFF', fontSize: 50, color: "#FFFFFF", }).setOrigin(0.5);
+        const startGameButton = this.add.text(positions.centerX, positions.centerY + 100, 'Start Game!', { fontFamily: 'Arial', fill: '#00ff00', fontSize: 50, color: "#00ff00", }).setOrigin(0.5);
         startGameButton.setInteractive({ useHandCursor: true })
         startGameButton.setDepth(10);
         
