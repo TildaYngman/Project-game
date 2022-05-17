ec33dd16f1e086415bd276c22c4f5d2588d85044
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-05-17 14:26:27 +0200

    removed physics import from phaser

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,24 @@
-import Phaser, { Physics } from 'phaser';// Phaser through node
+import Phaser from "phaser"; // Phaser through node
 
 // import Game from './Game';
-import Title from './title.js';
-import Game from './game.js';
-import GameOver from './GameOver.js';
+import Title from "./title.js";
+import Game from "./game.js";
+import GameOver from "./GameOver.js";
 // import GameOver from './GameOver.js';
 
-var config = {//What the gameboard is going to look like.
-    type: Phaser.AUTO,// will choose either webGL or canvas as renderer
-    width: 600, //height and width of the game board
-    height: 650,
-    scene:[Title, Game, GameOver], // run preload, create, update
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 2000 },
-            debug: false // turn this to true and you will get borders and direction "hit area of object"
-        }
-    }
+var config = {
+  //What the gameboard is going to look like.
+  type: Phaser.AUTO, // will choose either webGL or canvas as renderer
+  width: 600, //height and width of the game board
+  height: 650,
+  scene: [Title, Game, GameOver], // run preload, create, update
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 2000 },
+      debug: false, // turn this to true and you will get borders and direction "hit area of object"
+    },
+  },
 };
 
 var Game = new Phaser.Game(config);
